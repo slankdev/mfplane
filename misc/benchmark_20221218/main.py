4944dcb2d5cd61299ae2b900f68b1d773077ddb6
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-12-18 12:19:39 +0900

    2022.12.18-12:19

--- misc/benchmark_20221218/main.py
+++ misc/benchmark_20221218/main.py
@@ -15,38 +15,26 @@ tfs = 16
 datas = [
   "./t5_P10_b100M.json",
   "./t5_P10_b100M_4.json",
+  "./t5_P10_b100M_8.json",
 ]
 
 for data in datas:
-
-    data_obj = {}
     with open(data) as f:
         data_obj = json.load(f)
+        x = []
+        for interval in data_obj["intervals"]:
+            for stream in interval["streams"]:
+                x.append(stream["rtt"])
+        x = np.array(sorted(x))
+        cdf = np.cumsum(x) / np.sum(x)
+        plt.plot(x, cdf, label=data_obj["title"])
 
-    title = data_obj["title"]
-    x = []
-    for interval in data_obj["intervals"]:
-        for stream in interval["streams"]:
-            x.append(stream["rtt"])
-
-    x = sorted(x)
-    x = np.array(x)
-    cdf = np.cumsum(x) / np.sum(x)
-
-    plt.plot(x, cdf, label=title)
-
-# x = np.linspace(0,30,100)
-# for i in [1,3,6,9,15]:
-#     gamma_cdf = stats.gamma.cdf(x, i)
-#     plt.plot(x, gamma_cdf, label='a = {}'.format(i))
-# plt.legend(loc='best')
-
-#plt.plot(y, cdf, label="CDF")
-#plt.xticks(fontsize=tfs)
-#plt.yticks(fontsize=tfs)
-plt.xlabel("Maximum RTT (msec)", fontsize=fs)
+plt.xticks(fontsize=tfs)
+plt.yticks(fontsize=tfs)
+plt.xlabel("Maximum RTT (usec)", fontsize=fs)
 plt.ylabel("CDF", fontsize=fs)
-#plt.title("Scalability of MF-Plane (BW=50Mbps/N-node)", fontsize=fs)
+plt.ylim(0, 1)
+plt.title("CDF of RTT", fontsize=fs)
 plt.tight_layout()
 plt.legend(fontsize=tfs)
 plt.savefig("out.pdf")
