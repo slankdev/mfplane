b66c21dceaa41c8b735a8198eca6d3c5694362a7
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-12-18 12:12:34 +0900

    2022.12.18-12:12

--- misc/benchmark_20221218/main.py
+++ misc/benchmark_20221218/main.py
@@ -1,40 +1,51 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from scipy import stats
 import json
 import sys
 import pprint
+
+datas = [
+  "./t5_P10_b100M.json",
+]
 
 dd = {}
 with open("./t5_P10_b100M.json") as f:
     dd = json.load(f)
 
+title = dd["title"]
+print(title)
+
+x = []
 for interval in dd["intervals"]:
     for stream in interval["streams"]:
-        #pprint.pprint(stream)
-        print(stream["rtt"])
-        #sys.exit(0)
-    
-#pprint.pprint(dd["end"]["streams"])
-sys.exit(0)
+        x.append(stream["rtt"])
 
-data = pd.read_csv('./data.csv')
-x = data[data.keys()[0]]
-y1 = data[data.keys()[1]]
-y2 = data[data.keys()[2]]
+x = sorted(x)
+x = np.array(x)
+cdf = np.cumsum(x) / np.sum(x)
+
 fs = 18
 tfs = 16
 
 plt.close("all")
 plt.figure(figsize=(7, 4))
+plt.plot(x, cdf, label="CDF", marker=".")
 
-plt.plot(x, y1, label="downstream")
-plt.plot(x, y2, label="upstream")
-plt.xticks(fontsize=tfs)
-plt.yticks(fontsize=tfs)
-plt.xlabel("Number of N-nodes", fontsize=fs)
-plt.ylabel("Throughput (Mbps)", fontsize=fs)
-plt.title("Scalability of MF-Plane (BW=50Mbps/N-node)", fontsize=fs)
+# x = np.linspace(0,30,100)
+# for i in [1,3,6,9,15]:
+#     gamma_cdf = stats.gamma.cdf(x, i)
+#     plt.plot(x, gamma_cdf, label='a = {}'.format(i))
+# plt.legend(loc='best')
+
+#plt.plot(y, cdf, label="CDF")
+#plt.xticks(fontsize=tfs)
+#plt.yticks(fontsize=tfs)
+plt.xlabel("Maximum RTT (msec)", fontsize=fs)
+plt.ylabel("CDF", fontsize=fs)
+#plt.title("Scalability of MF-Plane (BW=50Mbps/N-node)", fontsize=fs)
 plt.tight_layout()
 plt.legend(fontsize=tfs)
-plt.savefig("mfplane_performance.pdf")
+plt.savefig("out.pdf")
