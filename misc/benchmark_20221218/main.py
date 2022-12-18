491c09c8019fbad70b4fc149b8c7e785a518a1ec
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-12-18 12:16:40 +0900

    2022.12.18-12:16

--- misc/benchmark_20221218/main.py
+++ misc/benchmark_20221218/main.py
@@ -7,32 +7,33 @@ import json
 import sys
 import pprint
 
-datas = [
-  "./t5_P10_b100M.json",
-]
-
-dd = {}
-with open("./t5_P10_b100M.json") as f:
-    dd = json.load(f)
-
-title = dd["title"]
-print(title)
-
-x = []
-for interval in dd["intervals"]:
-    for stream in interval["streams"]:
-        x.append(stream["rtt"])
-
-x = sorted(x)
-x = np.array(x)
-cdf = np.cumsum(x) / np.sum(x)
-
+plt.close("all")
+plt.figure(figsize=(7, 4))
 fs = 18
 tfs = 16
 
-plt.close("all")
-plt.figure(figsize=(7, 4))
-plt.plot(x, cdf, label="CDF", marker=".")
+datas = [
+  "./t5_P10_b100M.json",
+  "./t5_P10_b100M_4.json",
+]
+
+for data in datas:
+
+    data_obj = {}
+    with open(data) as f:
+        data_obj = json.load(f)
+
+    title = data_obj["title"]
+    x = []
+    for interval in data_obj["intervals"]:
+        for stream in interval["streams"]:
+            x.append(stream["rtt"])
+
+    x = sorted(x)
+    x = np.array(x)
+    cdf = np.cumsum(x) / np.sum(x)
+
+    plt.plot(x, cdf, label=title)
 
 # x = np.linspace(0,30,100)
 # for i in [1,3,6,9,15]:
