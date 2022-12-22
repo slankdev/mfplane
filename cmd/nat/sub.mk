6c9372cf5117460561d3673c1eaf137d8ff65f65
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-12-22 22:49:35 +0000

    2022.12.22-22:49

---
+++ cmd/nat/sub.mk
@@ -0,0 +1,24 @@
+MODE := xdpgeneric
+nat-build:
+	clang -target bpf -O3 -g -c cmd/nat/main.c -I cmd/nat -o ./bin/nat.o
+nat-off-all:
+	docker exec N1 ip link set net0 $(MODE) off || true
+	docker exec N2 ip link set net0 $(MODE) off || true
+	docker exec N3 ip link set net0 $(MODE) off || true
+nat-attach-n1: nat-build
+	docker cp ./bin/nat.o N1:/nat.o
+	docker exec N1 ip link set net0 $(MODE) off || true
+	docker exec N1 ip link set net0 $(MODE) obj /nat.o sec xdp-ingress
+nat-attach-n2: nat-build
+	docker cp ./bin/nat.o N2:/nat.o
+	docker exec N2 ip link set net0 $(MODE) off || true
+	docker exec N2 ip link set net0 $(MODE) obj /nat.o sec xdp-ingress
+nat-attach-n3: nat-build
+	docker cp ./bin/nat.o N2:/nat.o
+	docker exec N3 ip link set net0 $(MODE) off || true
+	docker exec N3 ip link set net0 $(MODE) obj /nat.o sec xdp-ingress
+nat-attach-all: nat-build \
+	nat-attach-n1 \
+	nat-attach-n2 \
+	nat-attach-n3 \
+	# END
