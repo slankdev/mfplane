a962627f3225868d2cba8d0d298ca85155456a58
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-12-23 10:23:29 +0000

    2022.12.23-10:23

--- cmd/clb/sub.mk
+++ cmd/clb/sub.mk
@@ -2,7 +2,7 @@ MODE := xdpgeneric
 clb-build:
 	clang -target bpf -O3 -g -c cmd/clb/main.c -I cmd/clb -o ./bin/clb.o
 
-clb-load:
+clb-load: clb-build
 	docker cp ./bin/clb.o L1:/clb.o
 	docker exec L1 ip link set net0 $(MODE) off || true
 	docker exec L1 ip link set net0 $(MODE) obj /clb.o sec xdp-ingress
