c48f9fada94ca7f52651bf10274b78926c6172ff
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-12-25 09:12:59 +0000

    2022.12.25-09:12

--- cmd/mikanectl/sub.mk
+++ cmd/mikanectl/sub.mk
@@ -10,7 +10,14 @@ nat-detach-all:
 	docker exec N1 mikanectl bpf nat detach -i net0
 	docker exec N2 mikanectl bpf nat detach -i net0
 	docker exec N3 mikanectl bpf nat detach -i net0
-nat-attach-all:
+nat-attach-all: \
+	nat-attach-n1 \
+	nat-attach-n2 \
+	nat-attach-n3 \
+	# END
+nat-attach-n1: mikanectl-install
 	docker exec N1 mikanectl bpf nat attach -i net0 -f
+nat-attach-n2: mikanectl-install
 	docker exec N2 mikanectl bpf nat attach -i net0 -f
+nat-attach-n3: mikanectl-install
 	docker exec N3 mikanectl bpf nat attach -i net0 -f
