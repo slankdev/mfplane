3982727bea757a0214f92ad66c6f1d09149b7f1a
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-12-25 09:45:02 +0000

    2022.12.25-09:45

--- cmd/mikanectl/sub.mk
+++ cmd/mikanectl/sub.mk
@@ -2,19 +2,6 @@ mikanectl-build:
 	CGO_ENABLED=0 go build -o bin/mikanectl cmd/mikanectl/main.go
 mikanectl-run: mikanectl-build
 	./bin/mikanectl hash
-mikanectl-install-n: mikanectl-build
-	docker cp ./bin/mikanectl N1:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl N2:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl N3:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl N4:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl N5:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl N6:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl N7:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl N8:/usr/bin/mikanectl
-mikanectl-install-l: mikanectl-build
-	docker cp ./bin/mikanectl L1:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl L2:/usr/bin/mikanectl
-	docker cp ./bin/mikanectl L3:/usr/bin/mikanectl
 nat-detach-all:
 	docker exec N1 mikanectl bpf nat detach -i net0
 	docker exec N2 mikanectl bpf nat detach -i net0
@@ -24,12 +11,15 @@ nat-attach-all: \
 	nat-attach-n2 \
 	nat-attach-n3 \
 	# END
-nat-attach-n1: mikanectl-install-n
+nat-attach-n1: mikanectl-build
+	docker cp ./bin/mikanectl N1:/usr/bin/mikanectl
 	docker exec N1 mikanectl bpf nat attach -i net0 -f
-nat-attach-n2: mikanectl-install-n
+nat-attach-n2: mikanectl-build
+	docker cp ./bin/mikanectl N2:/usr/bin/mikanectl
 	docker exec N2 mikanectl bpf nat attach -i net0 -f
-nat-attach-n3: mikanectl-install-n
+nat-attach-n3: mikanectl-build
+	docker cp ./bin/mikanectl N3:/usr/bin/mikanectl
 	docker exec N3 mikanectl bpf nat attach -i net0 -f
-
-clb-attach-l1: mikanectl-install-l
+clb-attach-l1: mikanectl-build
+	docker cp ./bin/mikanectl L1:/usr/bin/mikanectl
 	docker exec L1 mikanectl bpf clb attach -i net0 -f
