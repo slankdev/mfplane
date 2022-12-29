bbeb0e45444181c9f8a5638f6e5f9b3c53a65de4
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-12-29 12:21:31 +0000

    2022.12.29-12:21

--- setup.sh
+++ setup.sh
@@ -1,32 +1,8 @@
 #!/bin/sh
 set -xe
-
-## THIS WILL BE DELETED
-# mikanectl hash bpftoolcli -t 7 -b fc00:11:1::1 -n l1 | sudo sh -xe
-
 sudo mikanectl map-load -f l1.config.yaml
-# sudo mikanectl map-load -f l2.config.yaml
-# sudo mikanectl map-load -f l3.config.yaml
+sudo mikanectl map-load -f l2.config.yaml
+sudo mikanectl map-load -f l3.config.yaml
 sudo mikanectl map-load -f n1.config.yaml
 sudo mikanectl map-load -f n2.config.yaml
 sudo mikanectl map-load -f n3.config.yaml
-
-## THIS WILL BE DELETED
-# # fc00:1:1::/128 -> 0x80
-# sudo bpftool map update name l1_fib6 \
-# 	key hex \
-# 	80 00 00 00 \
-# 	fc 00 00 01 00 01 00 00 \
-# 	00 00 00 00 00 00 00 00 \
-# 	value hex \
-# 	80
-
-## THIS WILL BE DELETED
-# # fc00:1:1::/32 -> 0x20
-# sudo bpftool map update name l1_fib6 \
-# 	key hex \
-# 	20 00 00 00 \
-# 	fc 00 00 01 00 01 00 00 \
-# 	00 00 00 00 00 00 00 00 \
-# 	value hex \
-# 	20
