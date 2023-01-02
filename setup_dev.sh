3a4eadf3e6539dd5f30b635f9fec6b56f4469221
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2023-01-02 09:04:16 +0000

    STABLE 2023.01.02-09:04
    Signed-off-by: Hiroki Shirokura <hiroki.shirokura@linecorp.com>

--- setup_dev.sh
+++ setup_dev.sh
@@ -3,6 +3,8 @@ set -xe
 make clb-attach-l1
 make nat-attach-n1
 make nat-attach-n2
+make nat-attach-n3
 sudo mikanectl map-load -f l1.config.yaml
 sudo mikanectl map-load -f n1.config.yaml
 sudo mikanectl map-load -f n2.config.yaml
+sudo mikanectl map-load -f n3.config.yaml
