e2669a7d617b933e44b5fa6a33a53baae0a31eaa
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2023-01-04 01:50:36 +0000

    2023.01.04-01:50

--- pseudo_stun.sh
+++ pseudo_stun.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -xe
-docker exec VM1 sh -c "echo hoge | nc -w1 -u -p 8888 20.0.0.1 1111"
-docker exec VM1 sh -c "echo hoge | nc -w1 -u -p 9999 20.0.0.1 1111"
+docker exec VM1 sh -c "echo hoge | nc -w1 -u -p 10001 20.0.0.1 1111"
+docker exec VM1 sh -c "echo hoge | nc -w1 -u -p 10002 20.0.0.1 1111"
+docker exec VM2 sh -c "echo hoge | nc -w1 -u -p 10001 20.0.0.1 1111"
+docker exec VM2 sh -c "echo hoge | nc -w1 -u -p 10002 20.0.0.1 1111"
+docker exec VM3 sh -c "echo hoge | nc -w1 -u -p 10001 20.0.0.1 1111"
+docker exec VM3 sh -c "echo hoge | nc -w1 -u -p 10002 20.0.0.1 1111"
