dc442fc4ab2c5a0328f46a60ab6a6ad5570bed8f
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2023-02-19 16:34:34 +0900

    2023.02.19-16:34

--- misc/benchmark_20221217/main.py
+++ misc/benchmark_20221217/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+figsize#!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -10,15 +10,15 @@ fs = 18
 tfs = 16
 
 plt.close("all")
-plt.figure(figsize=(7, 4))
+plt.figure(figsize=(7, 2.5))
 
-plt.plot(x, y1, label="downstream")
-plt.plot(x, y2, label="upstream")
+plt.plot(x, y1, label="downstream -P20")
+plt.plot(x, y2, label="upstream -P20")
 plt.xticks(fontsize=tfs)
 plt.yticks(fontsize=tfs)
 plt.xlabel("Number of N-nodes", fontsize=fs)
-plt.ylabel("Throughput (Mbps)", fontsize=fs)
-plt.title("Scalability of MF-Plane (BW=50Mbps/N-node)", fontsize=fs)
+plt.ylabel("BW (Mbps)", fontsize=fs)
+#plt.title("Scalability of MF-Plane (BW=50Mbps/N-node)", fontsize=fs)
 plt.tight_layout()
 plt.legend(fontsize=tfs)
 plt.savefig("mfplane_performance.pdf")
