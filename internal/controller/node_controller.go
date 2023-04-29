e49bb11021eb0feac82144b5f6d04ab6919ccc91
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2023-04-29 22:59:51 +0000

    2023.04.29-22:59

--- internal/controller/node_controller.go
+++ internal/controller/node_controller.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 
+	"gopkg.in/yaml.v2"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -27,6 +28,7 @@ import (
 
 	mfplanev1alpha1 "github.com/slankdev/mfplane/api/v1alpha1"
 	"github.com/slankdev/mfplane/pkg/goroute2"
+	"github.com/slankdev/mfplane/pkg/mikanectl"
 	"github.com/slankdev/mfplane/pkg/util"
 )
 
@@ -57,7 +59,8 @@ func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	log.Info("RECONCILE")
+	// Reconcile1
+	log.Info("RECONCILE_XDP_ATTACHING")
 	for _, fn := range node.Spec.Functions {
 		util.SetLogger(log)
 
@@ -78,20 +81,72 @@ func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 				return ctrl.Result{}, err
 			}
 		}
+	}
+
+	// Reconcile1
+	log.Info("RECONCILE_XDP_ATTACHING")
+	for _, fn := range node.Status.Functions {
+		fnSpec := mfplanev1alpha1.FunctionSpec{}
+		if err := node.GetFunctionSpec(fn.Name, &fnSpec); err != nil {
+			return ctrl.Result{}, err
+		}
+		fnStatus := mfplanev1alpha1.FunctionStatus{}
+		if err := node.GetFunctionStatus(fn.Name, &fnStatus); err != nil {
+			return ctrl.Result{}, err
+		}
 
 		// Prepare config
+		configFile, err := craftConfig(fnSpec, fnStatus)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 		if err := util.WriteFile(fmt.Sprintf("/tmp/%s.config.yaml", fn.Name),
-			[]byte(fn.ConfigFile)); err != nil {
+			[]byte(configFile)); err != nil {
 			return ctrl.Result{}, err
 		}
 		if _, err := util.LocalExecutef("sudo ip netns exec %s "+
 			"./bin/mikanectl map-load -f /tmp/%s.config.yaml",
-			fn.Netns, fn.Name); err != nil {
+			fnSpec.Netns, fn.Name); err != nil {
 			return ctrl.Result{}, err
 		}
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func craftConfig(fnSpec mfplanev1alpha1.FunctionSpec,
+	fnStatus mfplanev1alpha1.FunctionStatus) (string, error) {
+	switch fnSpec.Type {
+	case "clb":
+		c := mikanectl.Config{
+			NamePrefix:  fnSpec.Name,
+			MaxRules:    2,
+			MaxBackends: 7,
+			EncapSource: fnSpec.SegmentRoutingSrv6.EncapSource,
+		}
+		out, err := yaml.Marshal(c)
+		if err != nil {
+			return "", err
+		}
+		sout := string(out) + "\n" + fnSpec.ConfigFile
+		return sout, nil
+	case "nat":
+		c := mikanectl.Config{
+			NamePrefix:  fnSpec.Name,
+			MaxRules:    2,
+			MaxBackends: 7,
+			EncapSource: fnSpec.SegmentRoutingSrv6.EncapSource,
+		}
+		out, err := yaml.Marshal(c)
+		if err != nil {
+			return "", err
+		}
+		sout := string(out) + "\n" + fnSpec.ConfigFile
+		return sout, nil
+	default:
+		return "", fmt.Errorf("unknown type %s", fnSpec.Type)
+	}
+	// return s, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
