9f853baba74fd0ae326aa1d61520f0025600ab30
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2023-04-30 12:39:49 +0000

    2023.04.30-12:39

--- internal/controller/srv6segment_controller.go
+++ internal/controller/srv6segment_controller.go
@@ -25,8 +25,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	"github.com/k0kubun/pp"
 	mfplanev1alpha1 "github.com/slankdev/mfplane/api/v1alpha1"
+	"github.com/slankdev/mfplane/pkg/util"
 )
 
 // Srv6SegmentReconciler reconciles a Srv6Segment object
@@ -38,7 +38,7 @@ type Srv6SegmentReconciler struct {
 type ScheduleCandidate struct {
 	NodeName string
 	FuncName string
-	// Segments []mfplanev1alpha1.Srv6Segment
+	Segments []mfplanev1alpha1.Srv6Segment
 }
 
 //+kubebuilder:rbac:groups=mfplane.mfplane.io,resources=srv6segments,verbs=get;list;watch;create;update;patch;delete
@@ -66,22 +66,56 @@ func (r *Srv6SegmentReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		return ctrl.Result{}, nil
 	}
 
+	specUpdated := false
+	statusUpdated := false
 	log.Info("RECONCILE_START")
-	funcType := "unknown"
-	switch {
-	case seg.Spec.EndMflNat != nil:
-		funcType = "clb"
-	case seg.Spec.EndMfnNat != nil:
-		funcType = "nat"
-	default:
-		return ctrl.Result{}, fmt.Errorf("no sid activated")
-	}
-	c0, err := GetScheduleCandidates(ctx, r.Client, funcType)
-	if err != nil {
-		return ctrl.Result{}, err
+
+	if seg.Status.NodeName == "" || seg.Status.FuncName == "" {
+		funcType := "unknown"
+		switch {
+		case seg.Spec.EndMflNat != nil:
+			funcType = "clb"
+		case seg.Spec.EndMfnNat != nil:
+			funcType = "nat"
+		default:
+			return ctrl.Result{}, fmt.Errorf("no sid activated")
+		}
+
+		// FILTERS
+		c0, err := GetScheduleCandidates(ctx, r.Client, funcType)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		c1, err := FilterAntiAffinity(ctx, r.Client, seg, c0)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// TODO(slankdev)
+		// sort.Slice(c0, func(i, j int) bool {
+		// 	return len(c0[i].Segments) < len(c0[j].Segments)
+		// })
+
+		// ELECT
+		if len(c1) > 0 {
+			c := c1[0]
+			seg.Status.NodeName = c.NodeName
+			seg.Status.FuncName = c.FuncName
+			statusUpdated = true
+		}
 	}
 
-	pp.Println(c0)
+	// Write back changes
+	if statusUpdated {
+		if err := r.Status().Update(ctx, &seg); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+	if specUpdated {
+		if err := r.Update(ctx, &seg); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
 	return ctrl.Result{}, nil
 }
 
@@ -98,17 +132,53 @@ func GetScheduleCandidates(ctx context.Context, cli client.Client,
 	if err := cli.List(ctx, &nodeList); err != nil {
 		return nil, err
 	}
+	segList := mfplanev1alpha1.Srv6SegmentList{}
+	if err := cli.List(ctx, &segList); err != nil {
+		return nil, err
+	}
+
 	candidates := []ScheduleCandidate{}
 	for _, node := range nodeList.Items {
 		for _, fn := range node.Spec.Functions {
 			if fn.Type == funcType {
-				c := ScheduleCandidate{
+				segs := []mfplanev1alpha1.Srv6Segment{}
+				for _, seg := range segList.Items {
+					if seg.Status.NodeName == node.Name &&
+						seg.Status.FuncName == fn.Name {
+						segs = append(segs, seg)
+					}
+				}
+				candidates = append(candidates, ScheduleCandidate{
 					NodeName: node.Name,
 					FuncName: fn.Name,
-				}
-				candidates = append(candidates, c)
+					Segments: segs,
+				})
 			}
 		}
 	}
 	return candidates, nil
+}
+
+func FilterAntiAffinity(ctx context.Context, cli client.Client,
+	seg mfplanev1alpha1.Srv6Segment,
+	in []ScheduleCandidate) ([]ScheduleCandidate, error) {
+	out := []ScheduleCandidate{}
+	for _, item := range in {
+		segList := mfplanev1alpha1.Srv6SegmentList{}
+		if err := mfplanev1alpha1.ListSegmentsNodeFunc(ctx, cli,
+			item.NodeName, item.FuncName, &segList); err != nil {
+			return nil, err
+		}
+		drop := false
+		for _, checkSeg := range segList.Items {
+			if util.VerifyMatchLabels(checkSeg.Labels, seg.Spec.Selector.MatchLabels) {
+				drop = true
+				break
+			}
+		}
+		if !drop {
+			out = append(out, item)
+		}
+	}
+	return out, nil
 }
