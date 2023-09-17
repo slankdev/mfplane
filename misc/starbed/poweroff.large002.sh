d3e47878fd67480e618a96821b09d362e778f913
Hiroki Shirokura <slank.dev@gmail.com>
2023-09-17 14:35:41 +0900

    2023.09.17-14:35

---
+++ misc/starbed/poweroff.large002.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xe
+
+starbedctl resource power off -n w011
+starbedctl resource power off -n w012
+starbedctl resource power off -n w013
+starbedctl resource power off -n w014
+starbedctl resource power off -n w015
+starbedctl resource power off -n w016
+starbedctl resource power off -n w017
+starbedctl resource power off -n w018
+starbedctl resource power off -n w019
+starbedctl resource power off -n w020
