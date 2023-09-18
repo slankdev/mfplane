f2ffb516a1e151ffbdbf26858d9b2aa76b66024e
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2023-09-18 02:46:09 +0000

    2023.09.18-02:46

---
+++ misc/starbed/scripts/hostTcpdump.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import sys
+import yaml
+import argparse
+import subprocess
+
+
+# Arg parse
+parser = argparse.ArgumentParser()
+parser.add_argument("node", help="specify network-node name")
+parser.add_argument("-i", "--inventory", default="hosts.large.yaml")
+args = parser.parse_args()
+
+# Load inventory file
+host = None
+with open(args.inventory, "r") as f:
+  obj = yaml.safe_load(f)
+  for c in obj["all"]["vars"]["containers"]:
+    if c["name"] == args.node:
+        host = c["host"]
+        break
+if host is None:
+  sys.exit("Error")
+
+# Execute remote login command
+cmd = f"docker -H ssh://{host} run --rm --privileged "
+cmd += f"--net host "
+cmd += f"nicolaka/netshoot "
+cmd += "tcpdump -qt -nni any -l "
+cmd += " not tcp port 22 and not arp and not tcp port 6443 and "
+cmd += "not tcp port 179 and not icmp6[0] == 135 and not icmp6[0] == 134"
+try:
+    subprocess.run(cmd, shell=True)
+except KeyboardInterrupt:
+    print(f"\n\nQUIT: {cmd}")
+    #raise
