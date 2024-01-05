8c8948168d462595773773ed87beffecaa420b86
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2024-01-05 09:09:27 +0000

    WIP
    Signed-off-by: Hiroki Shirokura <hiroki.shirokura@linecorp.com>
    
    DEV
    
    2024.02.21-02:36
    
    2024.02.21-02:39
    
    2024.02.21-02:47
    
    2024.02.21-03:42
    
    2024.02.21-22:24
    
    2024.02.21-22:25
    
    2024.02.21-22:30
    
    2024.02.21-22:39
    
    2024.02.21-22:55
    
    2024.02.21-23:00
    
    2024.02.21-23:01
    
    2024.02.21-23:09
    
    2024.02.21-23:12
    
    2024.02.21-23:13
    
    2024.02.21-23:16
    
    2024.02.21-23:17
    
    2024.02.21-23:19
    
    2024.02.21-23:23
    
    2024.02.21-23:24
    
    2024.02.21-23:42
    
    2024.02.23-17:11
    
    2024.02.23-17:11
    
    2024.02.23-19:39

--- cmd/mfpctl/sub.mk
+++ cmd/mfpctl/sub.mk
@@ -1,2 +1,8 @@
 mfpctl-build:
 	CGO_ENABLED=0 go build -o bin/mfpctl cmd/mfpctl/main.go
+
+mfpctl_img := slankdev/mfplane-mfpctl:develop
+mfpctl-docker-build:
+	docker build -t ${mfpctl_img} -f cmd/mfpctl/Dockerfile.tmp .
+mfpctl-docker-push: mfpctl-docker-build
+	docker push ${mfpctl_img}
