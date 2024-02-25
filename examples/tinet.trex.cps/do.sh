aed3a771ec1b41b8406a9fa557aa53f28fa5564d
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2024-02-25 13:10:56 +0900

    example n2

--- examples/tinet.trex.cps/do.sh
+++ examples/tinet.trex.cps/do.sh
@@ -13,4 +13,14 @@ sudo mfpctl bpf xdp attach common \
   --define DEBUG_FUNCTION_CALL_ \
   --define DEBUG_MF_REDIRECT_PACKET_RECORD \
   #END
+sudo mfpctl bpf xdp attach common \
+  --netns N2 --interface net0 --name n2 -v -f \
+  --define OVERLAY_FIB4_PREFIX_MASK=0xffff0000 \
+  --define NAT_CACHE_MAX_RULES=65535 \
+  --define DEBUG_ERROR_PACKET_ \
+  --define DEBUG_NAT_CONFLICT \
+  --define ENABLE_NAT_TCP_RST_CACHE_CLEAR \
+  --define DEBUG_FUNCTION_CALL_ \
+  --define DEBUG_MF_REDIRECT_PACKET_RECORD \
+  #END
 sudo mfpctl bpf map set-auto -f map.yaml
