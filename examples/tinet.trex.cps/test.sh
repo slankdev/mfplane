c7e2d87ebdffb45d71e232f29fcb05eef4884642
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2024-02-25 14:45:05 +0900

    2024.02.25-14:45

--- examples/tinet.trex.cps/test.sh
+++ examples/tinet.trex.cps/test.sh
@@ -1,3 +1,5 @@
+sudo mfpctl bpf map set-auto -f ops_n1.yaml
+sudo mfpctl bpf map set-auto -f counter_zero.json
 docker exec T1 mkdir -p /opt/trex/automation/trex_control_plane/interactive/trex/examples/astf
 docker cp ../../cmd/trex/new_connection_test.py T1:/opt/trex/automation/trex_control_plane/interactive/trex/examples/astf
 docker exec -it -w /opt/trex/automation/trex_control_plane/interactive/trex/examples/astf T1 python3 new_connection_test.py --test connect $@
